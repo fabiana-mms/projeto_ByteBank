b19df95dac4e710e2e66573468f87c4a8f33f3e0
Fabiana Monteiro <fabi_mms_05@hotmail.com>
2021-11-10 16:48:09 -0300

    Second commit

--- index.js
+++ index.js
@@ -1,18 +1,64 @@
-import {Cliente} from "./Cliente.js"
-import {ContaCorrente} from "./ContaCorrente.js"
+import { Cliente } from "./Cliente.js";
+import { Gerente } from "./Funcionario/Gerente.js";
+import { Diretor } from "./Funcionario/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
-const cliente1 = new Cliente("Fabiana", 11122233344);     
-const cliente2 = new Cliente("Ricardo", 44433322211);
+//Clientes
+const cliente = new Cliente("Sueli", 78945612312, "789456");
 
-const contaCorrenteRicardo = new ContaCorrente(1001, cliente1); 
-contaCorrenteRicardo.depositar(500);
+//Funcionarios
+const diretor = new Diretor("Fabiana", 10000, 12345678900);
+diretor.cadastrarSenha("123456");
+const gerente = new Gerente("Ricardo", 5000, 12345678911);
+gerente.cadastrarSenha("123654");
 
-const conta2 = new ContaCorrente(102, cliente2);
+//Clientes
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "789456");
 
-let valor = 200;
-contaCorrenteRicardo.transferir(valor, conta2);
+//Funcionarios
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
+const gerenteEstaLogado = SistemaAutenticacao.login(diretor, "123654");
 
-console.log(ContaCorrente.numeroDeContas);
+console.log(clienteEstaLogado);
+
+console.log(diretorEstaLogado);
+console.log(gerenteEstaLogado);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import {ContaCorrente} from "./Conta/ContaCorrente.js"
+// import {ContaPoupanca} from "./Conta/ContaPoupanca.js";
+// import {ContaSalario} from "./Conta/ContaSalario.js";
+
+// const cliente1 = new Cliente("Fabiana", 11122233344);     
+
+// const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001); 
+// contaCorrenteRicardo.depositar(500);
+// contaCorrenteRicardo.sacar(100);
+
+// const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+// contaPoupanca.depositar(850);
+// contaPoupanca.sacar(10);
+
+// const contaSalario = new ContaSalario(cliente1);
+// contaSalario.depositar(100);
+// contaSalario.sacar(50);
+
+// console.log(contaCorrenteRicardo);
+// console.log(contaPoupanca);
+// console.log(contaSalario);
 
 //O arquivo index.js fica sendo meu arquivo de execucao.
 
@@ -25,3 +71,4 @@ console.log(ContaCorrente.numeroDeContas);
 //Para que o index.js seja executado com seus arquivos de classes, deve-se no proprio terminal da o comando npm init, atrubuir package name e description e por onde comecar a leitura.
 //Yes no final e vera que um arquivo package.json sera criado.
 //Gerenciador de pacotes NPM Node package manager.
+//CTRL + J abre o terminal
